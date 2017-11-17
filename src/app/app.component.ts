8baaef3448ca5d50c080b521187fb37c7ebb5ef6
Mickael Benes <mickaelbenes@hotmail.fr>
2017-11-17 08:59:06 +0100

    Added the same animation on the update form and removed the undisplay of the create button on click.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,8 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 	styleUrls: ['./app.component.css'],
 	animations: [
 		trigger('formActivity', [
-			state( 'void', style( {height: 0} ) ),
-			state( '*', style( {height: 248} ) ),
+			state( 'void', style( {'max-height': 0} ) ),
+			state( '*', style( {'max-height': 248} ) ),
 			transition('void <=> *', animate('500ms ease-in') )
 		])
 	]
